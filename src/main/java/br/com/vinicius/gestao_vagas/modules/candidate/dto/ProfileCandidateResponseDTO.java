9be1c073efecd60d4b50554ddc63e9ef203f1577
Vinicius Aristimunha <viniciusarisp@gmail.com>
2024-02-28 15:22:29 -0300

    feat: Add profile candidate endpoint and response DTO

--- src/main/java/br/com/vinicius/gestao_vagas/modules/candidate/dto/ProfileCandidateResponseDTO.java
+++ src/main/java/br/com/vinicius/gestao_vagas/modules/candidate/dto/ProfileCandidateResponseDTO.java
@@ -2,6 +2,7 @@ package br.com.vinicius.gestao_vagas.modules.candidate.dto;
 
 import java.util.UUID;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -12,11 +13,19 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class ProfileCandidateResponseDTO {
-    
+
+    @Schema(example = "Desenvolvedor Java")
     private String description;
+
+    @Schema(example = "maria.java")
     private String username;
+
+    @Schema(example = "maria@gmail.com")
     private String email;
+    
     private UUID id;
+
+    @Schema(example = "Maria de souza")
     private String name;
 
 }
