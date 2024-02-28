c86b3dae8e5c771c4b57f634f416a9b57b6f9e48
Vinicius Aristimunha <viniciusarisp@gmail.com>
2024-02-28 16:47:58 -0300

    feat: Add custom exception classes for job and user not found

---
+++ src/main/java/br/com/vinicius/gestao_vagas/exceptions/JobNotFoundException.java
@@ -0,0 +1,7 @@
+package br.com.vinicius.gestao_vagas.exceptions;
+
+public class JobNotFoundException extends RuntimeException {
+    public JobNotFoundException() {
+        super("Job not found");
+    }
+}
