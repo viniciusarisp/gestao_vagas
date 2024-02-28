bacea67708cb6c1a74c5eb5464ced8008ddefbf5
Vinicius Aristimunha <viniciusarisp@gmail.com>
2024-02-28 16:15:44 -0300

    feat: Add API documentation for candidate controller

--- src/main/java/br/com/vinicius/gestao_vagas/modules/candidate/controllers/CandidateController.java
+++ src/main/java/br/com/vinicius/gestao_vagas/modules/candidate/controllers/CandidateController.java
@@ -32,6 +32,7 @@ import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/candidate")
+@Tag(name = "Candidato", description = "Informações do candidato")
 public class CandidateController {
 
 
@@ -46,6 +47,16 @@ public class CandidateController {
 
 
     @PostMapping("/")
+    @Operation(summary = "Cadastro de candidato",
+        description = "Essa função é responsável por cadastrar um candidato")
+    @ApiResponses({
+        @ApiResponse(responseCode = "200", content = {
+            @Content(
+                schema = @Schema(implementation =  CandidateEntity.class)
+            )
+        }),
+        @ApiResponse(responseCode = "400", description = "Usuário já existe")
+    })
     public ResponseEntity<Object> create(@Valid @RequestBody CandidateEntity candidateEntity) {
         try {
             var result = this.createCandidateUseCase.execute(candidateEntity);
@@ -57,7 +68,6 @@ public class CandidateController {
 
     @GetMapping("/")
     @PreAuthorize("hasRole('CANDIDATE')")
-    @Tag(name = "Candidato", description = "Informações do candidato")
     @Operation(summary = "Perfil do candidato",
         description = "Essa função é responsável por buscar as informações do perfil do candidato")
     @ApiResponses({
@@ -84,7 +94,6 @@ public class CandidateController {
 
     @GetMapping("/job")
     @PreAuthorize("hasRole('CANDIDATE')")
-    @Tag(name = "Candidato", description = "Informações do candidato")
     @Operation(summary = "Listagem de vagas disponíveis para o candidato",
         description = "Essa função é responsável por listar todas as vagas disponíveis baseada no filtro")
     @ApiResponses({
