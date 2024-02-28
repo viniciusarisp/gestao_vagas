99680284798f61df7c6d1ef984e22ec66f0e0778
Vinicius Aristimunha <viniciusarisp@gmail.com>
2024-02-28 16:15:32 -0300

    feat: Add Swagger annotations to CandidateEntity class

--- src/main/java/br/com/vinicius/gestao_vagas/modules/candidate/CandidateEntity.java
+++ src/main/java/br/com/vinicius/gestao_vagas/modules/candidate/CandidateEntity.java
@@ -6,6 +6,8 @@ import java.util.UUID;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.validator.constraints.Length;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.media.Schema.RequiredMode;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -23,18 +25,25 @@ public class CandidateEntity {
     @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
 
+    @Schema(example = "Vinicius Aristimunha Pinto", requiredMode = RequiredMode.REQUIRED, description = "Nome do candidato")
     private String name;
 
     @NotBlank()
     @Pattern(regexp = "\\S+", message = "O campo [username] não deve conter espaço")
+    @Schema(example = "viniciusari", requiredMode = RequiredMode.REQUIRED, description = "Usuário do candidato")
     private String username;
 
     @Email(message = "O campo [email] deve conter um e-mail válido")
+    @Schema(example = "vinicius@gmail.com", requiredMode = RequiredMode.REQUIRED, description = "E-mail do candidato")
     private String email;
 
     @Length(min = 10, max = 100, message = "O campo [password] deve conter entre 10 e 100 caracteres")
+    @Schema(example = "admin@123456789", minLength = 10, maxLength = 100, requiredMode = RequiredMode.REQUIRED, description = "Senha do candidato")
     private String password;
+
+    @Schema(example = "Desenvolvedor Front-End transicionando para FullStack", requiredMode = RequiredMode.REQUIRED, description = "Breve descrição do candidato")
     private String description;
+    
     private String curriculum; 
 
     @CreationTimestamp
