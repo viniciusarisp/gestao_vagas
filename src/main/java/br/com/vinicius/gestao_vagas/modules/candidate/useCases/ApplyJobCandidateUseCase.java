64bb48b684c4a8e61f1dd6fe6dc0b37ad6a89f99
Vinicius Aristimunha <viniciusarisp@gmail.com>
2024-02-28 16:47:21 -0300

    feat: Add ApplyJobCandidateUseCase class

---
+++ src/main/java/br/com/vinicius/gestao_vagas/modules/candidate/useCases/ApplyJobCandidateUseCase.java
@@ -0,0 +1,42 @@
+package br.com.vinicius.gestao_vagas.modules.candidate.useCases;
+
+import java.util.UUID;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.vinicius.gestao_vagas.exceptions.JobNotFoundException;
+import br.com.vinicius.gestao_vagas.exceptions.UserNotFoundException;
+import br.com.vinicius.gestao_vagas.modules.candidate.CandidateRepository;
+import br.com.vinicius.gestao_vagas.modules.company.repositories.JobRepository;
+
+@Service
+public class ApplyJobCandidateUseCase {
+
+    @Autowired
+    private JobRepository jobRepository;
+
+    @Autowired
+    private CandidateRepository candidateRepository;
+
+    //ID do candidato
+    //ID da vaga
+    public void execute(UUID idCandidate, UUID idJob) {
+        
+        //Validar se o candidato existe
+        this.candidateRepository.findById(idCandidate)
+        .orElseThrow(() -> {
+            throw new UserNotFoundException();
+        });
+
+        //Validar se a vaga existe
+        this.jobRepository.findById(idJob)
+        .orElseThrow(() -> {
+            throw new JobNotFoundException();
+        });
+
+        //Candidato se inscrever na vaga
+
+
+    }
+}
